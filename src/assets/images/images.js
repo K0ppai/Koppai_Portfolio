f038874398d6d387677f39b50560d270e57426e9
koppai <psthu.koppai@gmail.com>
2023-08-07 18:30:54 +0630

    Add project images

---
+++ src/assets/images/images.js
@@ -0,0 +1,9 @@
+import dota2Heros from './dota2_heros.png';
+import spaceTravellers from './space-travellers.png';
+import bookstore from './bookstore.png';
+import siribox from './siribox.png';
+import leaderboard from './leaderboard.png';
+import todo from './todo.png';
+import awesomeBooks from './awesome_books.png';
+
+export { dota2Heros, spaceTravellers, bookstore, siribox, leaderboard, todo, awesomeBooks };
