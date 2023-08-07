566d84cc42d3631f3394e4d65ee5bbda8da6de28
koppai <psthu.koppai@gmail.com>
2023-08-07 18:31:50 +0630

    Develop Navbar for mobile

--- src/components/NavBar/NavBarMobile.jsx
+++ src/components/NavBar/NavBarMobile.jsx
@@ -9,33 +9,50 @@ import {
 } from 'react-icons/bs';
 import { ImSun } from 'react-icons/im';
 import { AiFillHome, AiOutlineHome } from 'react-icons/ai';
+import PropTypes from 'prop-types';
 
-const NavBarMobile = ({ theme, toggleDarkMode }) => {
+const NavBarMobile = ({ darkMode, toggleDarkMode }) => {
   const path = useLocation().pathname;
 
   return (
-    <div className="fixed bottom-0 flex w-screen items-center justify-around bg-orange-300 py-2">
-      <NavLink to="/" className="flex flex-col items-center justify-center px-2">
-        {path === '/' ? <AiFillHome /> : <AiOutlineHome />}
-        <span>About Me</span>
+    <div className="fixed bottom-0 flex w-screen items-center justify-around bg-third_light py-2 text-text_light dark:bg-third_dark dark:text-text_dark rounded-tl-2xl rounded-tr-2xl md:hidden z-30">
+      <NavLink to="/" className="flex flex-col items-center justify-center">
+        {path === '/' ? (
+          <AiFillHome className="text-accent_light dark:text-accent_dark" />
+        ) : (
+          <AiOutlineHome className="text-accent_light dark:text-accent_dark" />
+        )}
+        <span>About</span>
       </NavLink>
-      <NavLink to="/projects" className="flex flex-col items-center justify-center px-2">
-        {path === '/projects' ? <BsFillGridFill /> : <BsGrid />}
+      <NavLink to="/projects" className="flex flex-col items-center justify-center">
+        {path === '/projects' ? (
+          <BsFillGridFill className="text-accent_light dark:text-accent_dark" />
+        ) : (
+          <BsGrid className="text-accent_light dark:text-accent_dark" />
+        )}
         <span>Projects</span>
       </NavLink>
-      <button className="flex flex-col items-center justify-center px-2" onClick={toggleDarkMode}>
-        {theme === 'dark' ? <BsFillMoonStarsFill /> : <ImSun />}
-        <span>Mode</span>
+      <button className="flex flex-col items-center justify-center" onClick={toggleDarkMode}>
+        {darkMode ? (
+          <BsFillMoonStarsFill className="text-accent_light dark:text-accent_dark" />
+        ) : (
+          <ImSun className="text-accent_light dark:text-accent_dark" />
+        )}
+        <span>{darkMode ? 'Dark' : 'Light'}</span>
       </button>
       <NavLink
         to="https://drive.google.com/file/d/1ulRSgcJ64u2OVebKbS7-22qyLPuJrHi4/view?usp=drive_link"
-        className="flex flex-col items-center justify-center px-2"
+        className="flex flex-col items-center justify-center"
       >
-        <BsFileEarmarkText />
+        <BsFileEarmarkText className="text-accent_light dark:text-accent_dark" />
         <span>Resume</span>
       </NavLink>
-      <NavLink to="/contact" className="flex flex-col items-center justify-center px-2">
-        {path === '/contact' ? <BsFillSendFill /> : <BsSend />}
+      <NavLink to="/contact" className="flex flex-col items-center justify-center">
+        {path === '/contact' ? (
+          <BsFillSendFill className="text-accent_light dark:text-accent_dark" />
+        ) : (
+          <BsSend className="text-accent_light dark:text-accent_dark" />
+        )}
         <span>Contact</span>
       </NavLink>
     </div>
@@ -43,3 +60,8 @@ const NavBarMobile = ({ theme, toggleDarkMode }) => {
 };
 
 export default NavBarMobile;
+
+NavBarMobile.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  toggleDarkMode: PropTypes.func.isRequired,
+};
