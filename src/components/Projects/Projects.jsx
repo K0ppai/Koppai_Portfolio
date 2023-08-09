ab3c100f5e5cc760de5fa0ef99dec8c33201c49e
koppai <psthu.koppai@gmail.com>
2023-08-09 22:28:06 +0630

    Change Horizontal Scroll for projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,5 @@
 import projectDatas from './ProjectDatas';
 import './Projects.css';
-import { v4 as id } from 'uuid';
 import { FiGithub } from 'react-icons/fi';
 import { RxExternalLink } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
@@ -8,54 +7,72 @@ import PropTypes from 'prop-types';
 
 const Projects = ({ darkMode }) => {
   return (
-    <div className="pb-20 pt-4">
-      <h1 className="text-center text-4xl font-bold">Show Room</h1>
-      <div className="grid grid-cols-1 gap-y-8 px-4 pt-4 md:grid-cols-4 md:gap-x-4">
+    <div className="px-4 pb-20 pt-4">
+      <div className="flex items-center py-4">
+        <h1 className="text-2xl text-gray-400">Projects</h1>
+        <div className="ml-2 h-[1px] w-[12vmin] bg-gray-400" />
+      </div>
+      <h2 className="text-4xl font-bold">Latest Work</h2>
+      <div className="horizontal-grid px-4 py-4">
         {projectDatas.map((projectData) => (
-          <div
+          <button
             key={projectData.id}
             className={`${
               darkMode ? 'shadow-drop-dark' : 'shadow-drop-light'
-            } flex flex-col overflow-hidden rounded-xl bg-third_light text-text_light dark:bg-bg_card_pale_dark dark:text-secondary_pale_light`}
+            } flex flex-col overflow-hidden rounded-xl bg-transparent text-text_light dark:bg-bg_card_pale_dark dark:text-secondary_pale_light`}
           >
-            <div className="mx-auto my-4 h-2 w-[25%] rounded bg-accent_dark dark:bg-bg_card_dark" />
-            <div className="relative">
-              <img src={projectData.image} alt="dota" className="block h-full" />
-              <div className="absolute inset-0 bg-black opacity-[60%]" />
-              <h2 className="absolute left-4 top-4 text-2xl font-bold text-accent_dark dark:text-secondary_pale_light">
-                {projectData.title}
-              </h2>
-              <h2 className="absolute right-4 top-4 text-sm text-accent_dark dark:text-secondary_pale_light">
-                {projectData.built_date}
-              </h2>
-              <div className="absolute bottom-4 right-4 flex w-[20%] justify-between">
-                <a href={projectData.github_link} target="_blank" rel="noopener noreferrer">
-                  <FiGithub className="text-2xl text-accent_dark dark:text-secondary_pale_light" />
-                </a>
-                <a href={projectData.live_link} target="_blank" rel="noopener noreferrer">
-                  <RxExternalLink className="text-2xl text-accent_dark dark:text-secondary_pale_light" />
-                </a>
+            <Link to={`/projects/${projectData.id}`} className="h-full">
+              {/* <div className="mx-auto my-4 h-1 w-[25%] rounded bg-accent_dark dark:bg-bg_card_dark" /> */}
+              <div className="relative h-full">
+                <img src={projectData.image} alt="dota" className="h-full" />
+                <div className="absolute inset-0 bg-black opacity-[60%]" />
+                <h2 className="absolute left-4 top-3 text-2xl font-bold text-accent_dark dark:text-secondary_pale_light">
+                  {projectData.title}
+                </h2>
+                <h2 className="absolute right-4 top-4 text-sm text-accent_dark dark:text-secondary_pale_light">
+                  {projectData.built_date}
+                </h2>
+                <div className="absolute bottom-4 right-4 flex w-[20%] justify-between">
+                  <a
+                    href={projectData.github_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <FiGithub className="text-2xl text-accent_dark dark:text-secondary_pale_light" />
+                  </a>
+                  <a
+                    href={projectData.live_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <RxExternalLink className="text-2xl text-accent_dark dark:text-secondary_pale_light" />
+                  </a>
+                </div>
+                <span className="absolute bottom-4 left-4 text-lg text-accent_dark dark:text-secondary_pale_light">
+                  {projectData.responsive}
+                </span>
               </div>
-              <span className="absolute bottom-4 left-4 text-lg text-accent_dark dark:text-secondary_pale_light">
-                {projectData.responsive}
-              </span>
-            </div>
-            <div className="flex flex-wrap items-center justify-center px-2 pt-2">
+              {/* Techs */}
+              {/* <div className="flex flex-wrap items-center justify-center px-2 py-3">
               {projectData.tech.map((tech) => (
                 <span
                   key={id()}
-                  className="mx-1 mb-2 rounded bg-accent_dark px-2 py-1 text-secondary_pale_light dark:bg-bg_card_dark dark:text-text_dark"
+                  className="mx-1 rounded px-2 py-1 font-bold text-accent_dark dark:bg-bg_card_dark dark:text-text_dark"
                 >
                   {tech}
                 </span>
               ))}
-            </div>
-            <div className="flex justify-center">
-              <button className="my-4 rounded-md bg-primary_light px-12 py-3 text-lg font-bold text-secondary_pale_light dark:bg-secondary_dark dark:text-secondary_pale_light">
+            </div> */}
+              {/* See More Btn */}
+              {/* <div className="flex justify-center">
+              <button className="my-4 rounded-md bg-primary_light px-12 pb-3 text-lg font-bold text-secondary_pale_light dark:bg-secondary_dark dark:text-secondary_pale_light">
                 <Link to={`/projects/${projectData.id}`}>See More</Link>
               </button>
-            </div>
-          </div>
+            </div> */}
+            </Link>
+          </button>
         ))}
       </div>
     </div>
