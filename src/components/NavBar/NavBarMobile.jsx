15c3375a494a66b9ac2e850c1e5ccd9b0013dea2
koppai <psthu.koppai@gmail.com>
2023-08-14 00:08:38 +0630

    Change navbar to a tags for sections views

--- src/components/NavBar/NavBarMobile.jsx
+++ src/components/NavBar/NavBarMobile.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import {
   BsFileEarmarkText,
   BsFillMoonStarsFill,
@@ -10,56 +10,103 @@ import {
 import { ImSun } from 'react-icons/im';
 import { AiFillHome, AiOutlineHome } from 'react-icons/ai';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 const NavBarMobile = ({ darkMode, toggleDarkMode }) => {
+  const [activeSection, setActiveSection] = useState('about-me');
   const path = useLocation().pathname;
+  const navigate = useNavigate();
+  const sections = ['about-me', 'projects', 'contact'];
+  
+  
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setActiveSection(entry.target.id);
+      }
+    });
+  };
+  
+  const handleNavigation = (sectionId) => {
+    if (path.startsWith('/projects/')) {
+      navigate('/');
+    }
+    const sectionElement = document.getElementById(sectionId);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+  
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      rootMargin: '-50% 0px -50% 0px',
+    });
+
+    sections.forEach((section) => {
+      const target = document.getElementById(section);
+      if (target) {
+        observer.observe(target);
+      }
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [sections]);
 
   return (
     <div className="flex justify-center">
       <div
         className={`${
           darkMode ? 'shadow-drop-dark' : 'shadow-drop-light'
-        } fixed bottom-4 z-30 flex w-[85vw] items-center justify-around rounded-3xl bg-white bg-opacity-40 py-5 px-2  text-text_light dark:bg-bg_dark dark:text-text_dark md:hidden`}
+        } fixed bottom-4 z-30 flex w-[85vw] items-center justify-around rounded-3xl bg-white bg-opacity-40 px-2 py-5  text-text_light dark:bg-bg_dark dark:text-text_dark md:hidden`}
       >
-        <NavLink to="/" className="flex flex-col items-center justify-center">
-          {path === '/' ? (
+        <a
+          href="#about-me"
+          className="flex flex-col items-center justify-center"
+          onClick={() => handleNavigation('about-me')}
+        >
+          {activeSection === 'about-me' ? (
             <AiFillHome className="text-2xl text-primary_dark" />
           ) : (
             <AiOutlineHome className="text-2xl text-primary_dark" />
           )}
-          {/* <span className='text-bg_card_pale_dark dark:text-secondary_pale_light'>About</span> */}
-        </NavLink>
-        <NavLink to="/projects" className="flex flex-col items-center justify-center">
-          {path === '/projects' ? (
+        </a>
+        <a
+          href="#projects"
+          className="flex flex-col items-center justify-center"
+          onClick={() => handleNavigation('projects')}
+        >
+          {activeSection === 'projects' ? (
             <BsFillGridFill className="text-2xl text-primary_dark" />
           ) : (
             <BsGrid className="text-2xl text-primary_dark" />
           )}
-          {/* <span className='text-bg_card_pale_dark dark:text-secondary_pale_light'>Projects</span> */}
-        </NavLink>
+        </a>
         <button className="flex flex-col items-center justify-center" onClick={toggleDarkMode}>
           {darkMode ? (
             <BsFillMoonStarsFill className="text-2xl text-primary_dark" />
           ) : (
             <ImSun className="text-2xl text-primary_dark" />
           )}
-          {/* <span className='text-bg_card_pale_dark dark:text-secondary_pale_light'>{darkMode ? 'Dark' : 'Light'}</span> */}
         </button>
         <NavLink
           to="https://drive.google.com/file/d/1ulRSgcJ64u2OVebKbS7-22qyLPuJrHi4/view?usp=drive_link"
           className="flex flex-col items-center justify-center"
         >
           <BsFileEarmarkText className="text-2xl text-primary_dark" />
-          {/* <span className='text-bg_card_pale_dark dark:text-secondary_pale_light'>Resume</span> */}
         </NavLink>
-        <NavLink to="/contact" className="flex flex-col items-center justify-center">
-          {path === '/contact' ? (
+        <a
+          href="#contact"
+          className="flex flex-col items-center justify-center"
+          onClick={() => handleNavigation('contact')}
+        >
+          {activeSection === 'contact' ? (
             <BsFillSendFill className="text-2xl text-primary_dark" />
           ) : (
             <BsSend className="text-2xl text-primary_dark" />
           )}
-          {/* <span className='text-bg_card_pale_dark dark:text-secondary_pale_light'>Contact</span> */}
-        </NavLink>
+        </a>
       </div>
     </div>
   );
