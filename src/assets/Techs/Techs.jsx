0fd385693f3000790d243b6df2c183fa4ade1e6b
koppai <psthu.koppai@gmail.com>
2023-08-19 17:33:02 +0630

    Insert links for connect component

--- src/assets/Techs/Techs.jsx
+++ src/assets/Techs/Techs.jsx
@@ -15,90 +15,42 @@ const techs = [
   {
     id: id(),
     originalElement: <SiReact className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-        alt="react"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiReact className="rotate-in-center text-[20vmin] text-[#00D8FF]" />,
   },
   {
     id: id(),
     originalElement: <SiRedux className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
-        alt="redux"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiRedux className="rotate-in-center text-[20vmin] text-[#764ABC]" />,
   },
   {
     id: id(),
     originalElement: <SiJavascript className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
-        alt="javascript"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiJavascript className="rotate-in-center text-[20vmin] text-[#F7DF1E]" />,
   },
   {
     id: id(),
     originalElement: <SiHtml5 className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
-        alt="html5"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiHtml5 className="rotate-in-center text-[20vmin] text-[#e44f26]" />,
   },
   {
     id: id(),
     originalElement: <SiCss3 className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
-        alt="css3"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiCss3 className="rotate-in-center text-[20vmin] text-[#1572B6]" />,
   },
   {
     id: id(),
     originalElement: <SiTailwindcss className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
-        alt="tailwindcss"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiTailwindcss className="rotate-in-center text-[20vmin] text-[#06B6D4]" />,
   },
   {
     id: id(),
     originalElement: <SiBootstrap className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg"
-        alt="bootstrap"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiBootstrap className="rotate-in-center text-[20vmin] text-[#7E13F8]" />,
   },
   {
     id: id(),
     originalElement: <SiGit className="text-[20vmin]" />,
-    hoverElement: (
-      <img
-        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg"
-        alt="github"
-        className="rotate-in-center w-[20vmin]"
-      />
-    ),
+    hoverElement: <SiGit className="rotate-in-center text-[20vmin] text-[#dd4c35]" />,
   },
   {
     id: id(),
